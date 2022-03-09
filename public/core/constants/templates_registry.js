4805ee2962a8c3ed4bcb053954935619ceda2c0c
senago <mirzoyan.oganes@mail.ru>
2022-03-09 14:01:08 +0300

    Cleanup

--- public/core/constants/templates_registry.js
+++ public/core/constants/templates_registry.js
@@ -1,11 +1,13 @@
 import '../../precompiled/Header.js';
 import '../../precompiled/Signup.js';
+import '../../precompiled/Login.js';
 import '../../precompiled/Feed.js';
 import '../../precompiled/PostSettings.js';
+import '../../precompiled/NotFound.js';
 
 export const TemplatesRegistry = {
   Header: Handlebars.templates.Header,
-  HeaderSettings: Handlebars.templates.HeaderSettings,
+  // HeaderSettings: Handlebars.templates.HeaderSettings,
 
   Login: Handlebars.templates.Login,
   NotFound: Handlebars.templates.NotFound,
