98a6669220946b0f1021ac9832c1d61189adedd2
senago <mirzoyan.oganes@mail.ru>
2022-03-15 13:25:24 +0300

    Fix Docker run

--- scripts/local_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp nginx/nginx.conf /etc/nginx/nginx.conf
-cp nginx/default.conf /etc/nginx/sites-available/default.conf
-ln -sf /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
-bash scripts/pack.sh
-bash scripts/precompile.sh
-rsync -az --delete src/ /var/www/web
-# nginx -s reload
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# nameNetwrokApi=$(docker network ls | grep mongo-go | awk '{{print $2}}')
-# docker run -d -p 80:80 --network $nameNetwrokApi --name=cj-frontend cj-frontend:latest
 docker run -d -p 80:80 --name=cj-frontend cj-frontend:latest
 
 if  echo $(curl -v --stderr - http://localhost) | grep  -q "Failed to connect" ; then
