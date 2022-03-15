8491fbfd997f7665ce6b416aafe0258b0b0b978d
senago <mirzoyan.oganes@mail.ru>
2022-03-15 13:00:13 +0300

    Fix

--- scripts/local_macos.sh
+++ scripts/local_macos.sh
@@ -3,5 +3,7 @@
 cp nginx/nginx.conf /etc/nginx/nginx.conf
 cp nginx/default.conf /etc/nginx/sites-available/default.conf
 ln -sf /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+bash scripts/pack.sh
+bash scripts/precompile.sh
 rsync -az --delete src/ /var/www/web
 # nginx -s reload
