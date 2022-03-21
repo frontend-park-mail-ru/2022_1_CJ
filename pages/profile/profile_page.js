4744ab001a80389d87af4f9075f534090aafd632
Similization <borbri228@gmail.com>
2022-03-22 00:22:49 +0300

    lil logic & glass

--- pages/profile/profile_page.js
+++ pages/profile/profile_page.js
@@ -1,42 +1,25 @@
-(function() {
-var a = document.querySelector('#aside1'), b = null, P = 0;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
-window.addEventListener('scroll', Ascroll, false);
-document.body.addEventListener('scroll', Ascroll, false);
-function Ascroll() {
-  if (b == null) {
-    var Sa = getComputedStyle(a, ''), s = '';
-    for (var i = 0; i < Sa.length; i++) {
-      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-      }
+document.addEventListener("click", function(e) {
+  const menu = document.querySelectorAll('.menu')[0];
+  profile = document.querySelectorAll('.profile')[0];
+
+  const withinBoundaries = e.composedPath().includes(profile);
+  if ( ! withinBoundaries ) {
+    if (menu.style.display == "block" && e.currentTarget != profile) {
+      profile.style.display = "flex"
+      menu.style.display = "none"
+      console.log("menu was hided");
     }
-    b = document.createElement('div');
-    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-    a.insertBefore(b, a.firstChild);
-    var l = a.childNodes.length;
-    for (var i = 1; i < l; i++) {
-      b.appendChild(a.childNodes[1]);
-    }
-    a.style.height = b.getBoundingClientRect().height + 'px';
-    a.style.padding = '0';
-    a.style.border = '0';
-  }
-  var Ra = a.getBoundingClientRect(),
-      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('footer').getBoundingClientRect().top + 0);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
-  if ((Ra.top - P) <= 0) {
-    if ((Ra.top - P) <= R) {
-      b.className = 'stop';
-      b.style.top = - R +'px';
-    } else {
-      b.className = 'sticky';
-      b.style.top = P + 'px';
-    }
+	}
+})
+
+document.querySelectorAll('.profile')[0].addEventListener("click", function(e) {
+  console.log(e.currentTarget);
+  menu = e.currentTarget.nextElementSibling;
+  if (!menu.style.display || menu.style.display == "none") {
+    e.currentTarget.style.display = "none"
+    menu.style.display = "block"
+    console.log("menu was opened");
   } else {
-    b.className = '';
-    b.style.top = '';
+    console.log(menu.style.display)
   }
-  window.addEventListener('resize', function() {
-    a.children[0].style.width = getComputedStyle(a, '').width
-  }, false);
-}
-})()
+});
