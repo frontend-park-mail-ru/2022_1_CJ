1237c86ca8d35cc09a10988db187de8220477187
senago <mirzoyan.oganes@mail.ru>
2022-03-23 11:13:57 +0300

    Remove redundant log

--- src/core/models/Store/Store.js
+++ src/core/models/Store/Store.js
@@ -18,9 +18,7 @@ export const createStore = (reducer, initialState, enhancer = null) => {
 
   store.dispatch = (action) => {
     store.state = reducer(store.state, action);
-    console.log('xxxxxxxxxxxxx');
     store.listeners.forEach((listener) => listener(store.state));
-    console.log('yyyyyyyyy');
   };
 
   return store;
