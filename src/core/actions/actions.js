ca46bd2c6d33c64d9c7acab7a7a5d7c864dc921d
senago <mirzoyan.oganes@mail.ru>
2022-03-26 14:51:39 +0300

    Cleanup

--- src/core/actions/actions.js
+++
@@ -1,8 +0,0 @@
-import { URL } from '../constants/constants.js';
-import { Router } from '../modules/Router/Router.js';
-import { AuthAPI } from '../network/api/auth.js';
-
-export const LogoutAction = async () => {
-  await AuthAPI.LogoutUser();
-  Router.navigateTo(URL.Login);
-};
