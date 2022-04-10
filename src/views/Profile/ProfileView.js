d118d8f924bb774d88f4af7e6883efc8f3c91f8a
Similization <borbri228@gmail.com>
2022-04-11 01:09:09 +0300

    commented nginx fixes

--- src/views/Profile/ProfileView.js
+++ src/views/Profile/ProfileView.js
@@ -1,8 +1,49 @@
 import { createView } from '../../core/models/View/View.js';
+import { postAPI } from '../../core/network/api/post.js';
+import { createPostDTO } from '../../core/network/dto/post.js';
+import { store, thunks } from '../../store/store.js';
+import { setStyleDisplayGrid } from '../../test/baseFunction.js';
+
+/**
+ * 
+ * @param {Event} e
+ */
+const showAddPostElement = (e) => {
+    let profileCreatePost = document.querySelector('.profile-create-post');
+    setStyleDisplayGrid(profileCreatePost);
+}
+
+/**
+ * 
+ * @param {Event} e 
+ */
+const uploadNewPost = (e) => {
+    let newPost = document.querySelector('.profile-create-post');
+    let text = newPost.querySelector('.textarea');
+    let postImages = newPost.querySelectorAll('.profile-create-post-images img');
+    let images = []; 
+    postImages.forEach((element) => {
+        images.push(element.src);
+    }) 
+
+    store.dispatch(
+        postAPI.createPost( 
+            createPostDTO(
+                text.innerHTML,
+                images,
+            )
+        )
+    );
+}
 
 const reducer = {
     onShow: () => {
-        console.log("BARABARA");
+        let postAdd = document.querySelector('.post-add');
+        postAdd.addEventListener("click", showAddPostElement);
+
+        let newPost = document.querySelector('.profile-create-post');
+        let uploadNewPostBtn = newPost.querySelector('.block.btn-primary');
+        uploadNewPostBtn.addEventListener("click", uploadNewPost);
     }
   };
 
