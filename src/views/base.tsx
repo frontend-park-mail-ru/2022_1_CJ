eabd69f3cb40e011c2735cb10e5d427e07cb4891
senago <mirzoyan.oganes@mail.ru>
2022-05-02 20:14:24 +0300

    Fix layout

--- src/views/base.tsx
+++ src/views/base.tsx
@@ -1,7 +1,31 @@
 import { treact } from "@treact";
-import { AuthMiddleware } from "src/components/@middlewares/auth";
+import { navigateTo } from "src/components/@helpers/router";
 import { Component } from "src/components/@types/component";
+import { URL } from "src/constants/constants";
+import { userAPI } from "src/core/network/api/user";
+import { UserStatus, useUserStore } from "src/stores/user";
 
 export const Base: Component = () => {
-	return <AuthMiddleware />;
+	const [userStore, setUserStore] = useUserStore();
+
+	treact.useEffect(() => {
+		userAPI.getUserData().then(
+			(response) => {
+				setUserStore({ ...userStore, user: response.user, status: UserStatus.Authorized });
+			},
+			() => {
+				setUserStore({ ...userStore, user: null, status: UserStatus.Unauthorized });
+			}
+		);
+	}, []);
+
+	if (userStore.status !== UserStatus.Unset) {
+		if (userStore.status === UserStatus.Authorized) {
+			navigateTo(URL.Feed);
+		} else {
+			navigateTo(URL.Login);
+		}
+	}
+
+	return null;
 };
