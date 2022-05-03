04f54ce157e964d4af9b3bc08f7320a15e2efea0
senago <mirzoyan.oganes@mail.ru>
2022-05-03 17:54:13 +0300

    Change createPost logic

--- src/components/profile/createPost.tsx
+++ src/components/profile/createPost.tsx
@@ -1,5 +1,7 @@
 import { treact } from "@treact";
+import { navigateTo } from "src/components/@helpers/router";
 import { Component } from "src/components/@types/component";
+import { URL } from "src/constants/constants";
 import { EventWithTarget } from "src/core/@types/event";
 import { postAPI } from "src/core/network/api/post";
 
@@ -12,15 +14,15 @@ export const CreatePost: Component = () => {
 	};
 
 	const createPost = () => {
-		if (show) {
-			postAPI.createPost({ message });
+		if (show && message.length > 0) {
+			postAPI.createPost({ message }).then(() => navigateTo(URL.Feed));
 		} else {
 			setShow(true);
 		}
 	};
 
 	return (
-		<div className="post flex flex-c bg-white pd-8">
+		<div className="post flex flex-c bg-white pd-8 border-8">
 			{show && <span onKeyUp={handleChange} contentEditable />}
 			<button onClick={createPost} className="btn btn-transparent d-middle">
 				Create post
