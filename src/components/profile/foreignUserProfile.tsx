00a5f5b96a7dfffae0c8b93437950c1398ab77de
senago <mirzoyan.oganes@mail.ru>
2022-05-03 21:13:31 +0300

    Add redirection

--- src/components/profile/foreignUserProfile.tsx
+++ src/components/profile/foreignUserProfile.tsx
@@ -1,6 +1,8 @@
 import { treact } from "@treact";
+import { navigateTo } from "src/components/@helpers/router";
 import { Component } from "src/components/@types/component";
 import { FriendButton } from "src/components/profile/friendButton";
+import { URL } from "src/constants/constants";
 import { User } from "src/core/@types/user";
 import { messengerAPI } from "src/core/network/api/messenger";
 import { userAPI } from "src/core/network/api/user";
@@ -20,7 +22,10 @@ export const ForeignUserProfileInfo: Component = ({ user_id }: { user_id: string
 				name: `${userStore.user.name.first}~${user.name.first}`,
 				author_ids: [user.id],
 			})
-			.then((response) => console.log(response.dialog_id));
+			.then(
+				(response) => console.log(response.dialog_id),
+				() => navigateTo(URL.Messenger)
+			);
 	};
 
 	if (user) {
