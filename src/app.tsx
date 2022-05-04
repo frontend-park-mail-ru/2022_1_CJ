2b0e5b11ef834a0db9f25f9e707936f684acab04
senago <mirzoyan.oganes@mail.ru>
2022-05-04 10:46:36 +0300

    Refactor Views layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,22 @@
 import { treact } from "@treact";
-import { Communities } from "src/views/communities";
-import { Community } from "src/views/community";
-import { CommunityCreate } from "src/views/communityCreate";
-import { ProfileSettings } from "src/views/profileSettings";
+import { Communities } from "src/views/communities/communities";
+import { Community } from "src/views/communities/community";
+import { CommunityCreate } from "src/views/communities/communityCreate";
+import { ProfileSettings } from "src/views/profile/profileSettings";
 import { Component } from "./components/@types/component";
 import { Logout } from "./components/logout";
 import { Route } from "./components/route";
 import { Router } from "./components/router";
 import { URL } from "./constants/constants";
 import { Base } from "./views/base";
-import { MessengerDialog } from "./views/dialog";
+import { MessengerDialog } from "./views/messenger/dialog";
 import { Feed } from "./views/feed";
 import { Friends } from "./views/friends";
-import { Login } from "./views/login";
-import { Messenger } from "./views/messenger";
+import { Login } from "./views/auth/login";
+import { Messenger } from "./views/messenger/messenger";
 import { NotFound } from "./views/notFound";
-import { Profile } from "./views/profile";
-import { Signup } from "./views/signup";
+import { Profile } from "./views/profile/profile";
+import { Signup } from "./views/auth/signup";
 
 export const App: Component = () => {
 	return (
