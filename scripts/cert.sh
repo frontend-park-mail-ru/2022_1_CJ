05cbadb2c8a50c18a3af4b74d6080952d7a30a16
senago <mirzoyan.oganes@mail.ru>
2022-05-21 17:02:37 +0300

    Change domain

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(senago.software)
+domains=(cj.senago.software)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
