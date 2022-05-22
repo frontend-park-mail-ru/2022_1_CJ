f941526c2d468dd42e4a57c8dccf3de53241b1aa
senago <mirzoyan.oganes@mail.ru>
2022-05-22 21:13:24 +0300

    Improve design

--- src/views/offline.tsx
+++ src/views/offline.tsx
@@ -5,7 +5,7 @@ export const Offline: Component = () => {
 	return (
 		<div className="offline flex grow items-center">
 			<div
-				className="d-middle bg-white pd-8 border-md text-primary bold"
+				className="d-middle bg-white pd-8 border-md text-primary text-center bold"
 				style="font-size: 3rem; width: clamp(15rem, 75%, 30rem);"
 			>
 				You're offline
