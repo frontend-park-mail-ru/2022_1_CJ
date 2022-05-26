05084f1b640f1ad87ff58ca5bbf72868ae2deaf9
senago <mirzoyan.oganes@mail.ru>
2022-05-26 14:19:53 +0300

    Change workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,10 @@
 module.exports = {
+	mode: "production",
 	globDirectory: "dist",
 	globPatterns: ["**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}"],
 	swDest: "dist/sw.js",
 	clientsClaim: true,
 	skipWaiting: true,
-	navigateFallback: "/index.html",
+	//navigateFallback: "/index.html",
+	//navigateFallbackDenylist: [/^((?!api).)*$/],
 };
